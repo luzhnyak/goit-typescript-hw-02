f700bed3a6de1fc28cb5e2f6e9bafdecde26e6d1
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2023-11-09 15:26:04 +0200

    finish generic

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -3,13 +3,13 @@
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
 enum Day {
-  Monday,
-  Tuesday,
-  Wednesday,
-  Thursday,
-  Friday,
-  Saturday,
-  Sunday,
+  Monday = "monday",
+  Tuesday = "tuesday",
+  Wednesday = "wednesday",
+  Thursday = "thursday",
+  Friday = "friday",
+  Saturday = "saturday",
+  Sunday = "sunday",
 }
 
 function isWeekend(day: Day): boolean {
